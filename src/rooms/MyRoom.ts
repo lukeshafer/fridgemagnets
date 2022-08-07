860b7f90128a216f71d33deebf44fc3c08454f85
Luke Shafer <lukealanshafer@gmail.com>
2022-08-06 19:43:51 -0500

    Updated game state - can now start a game (but can't finish yet)

--- src/rooms/MyRoom.ts
+++ src/rooms/MyRoom.ts
@@ -2,15 +2,57 @@ import { Room, Client } from 'colyseus';
 import { MyRoomState, Player } from './schema/MyRoomState';
 
 export class MyRoom extends Room<MyRoomState> {
+	// default options
+	maxClients = 6;
+	handSize = 25;
+	currentPrompt = '';
+
 	onCreate(options: any) {
+		// Setting options, or leaving defaults
+		this.handSize = options.handSize || this.handSize;
+		this.maxClients = options.maxClients || this.maxClients;
+
 		this.setState(new MyRoomState());
 
-		this.onMessage('type', (client, message) => {
-			//
-			// handle "type" message
-			//
+		console.log('room', this.roomId, 'created!', options);
+
+		this.onMessage('start', (client, message) => {
+			// if client is vip and there are at least 3 players, call startGame()
+			if (
+				this.state.players.get(client.sessionId).isVIP &&
+				this.state.players.size >= 1
+			) {
+				this.startGame();
+			} else {
+				console.log('Not enough players to start the game!');
+			}
 		});
-    
+
+		this.onMessage('updatePlayer', (client, message) => {
+			if (message.status) {
+				this.state.players.get(client.sessionId).status = message.status;
+			}
+		});
+	}
+
+	startGame() {
+		this.lock();
+		this.state.deck.shuffle();
+		this.dealHands();
+		this.state.gamePhase = 'playing';
+		this.state.players.forEach((player) => {
+			player.status = 'editing';
+		});
+		this.state.currentPrompt = this.state.PromptDeck.draw();
+	}
+
+	dealHands() {
+		for (let i = 0; i < this.handSize; i++) {
+			this.state.players.forEach((player) => {
+				let card = this.state.deck.deal();
+				player.hand.push(card);
+			});
+		}
 	}
 
 	onJoin(client: Client, options: any) {
@@ -18,6 +60,12 @@ export class MyRoom extends Room<MyRoomState> {
 
 		// create Player instance
 		const player = new Player();
+		player.name = options.name;
+
+		// if no players yet, set this one as VIP
+		if (this.state.players.size === 0) {
+			player.isVIP = true;
+		}
 
 		// place player in the map of players by its sessionId
 		// (client.sessionId is unique per connection!)
@@ -26,6 +74,11 @@ export class MyRoom extends Room<MyRoomState> {
 
 	onLeave(client: Client, consented: boolean) {
 		console.log(client.sessionId, 'left!');
+
+		// if player is VIP, assign new VIP
+		if (this.state.players.get(client.sessionId).isVIP) {
+			[...this.state.players][1][1].isVIP = true;
+		}
 
 		this.state.players.delete(client.sessionId);
 	}
--- src/rooms/schema/MyRoomState.ts
+++ src/rooms/schema/MyRoomState.ts
@@ -1,10 +1,6 @@
-import {
-	Schema,
-	Context,
-	type,
-	MapSchema,
-	ArraySchema,
-} from '@colyseus/schema';
+import { Schema, type, MapSchema, ArraySchema } from '@colyseus/schema';
+import words from '../../data/words.json';
+import prompts from '../../data/prompts.json';
 
 export class Piece extends Schema {
 	@type('string') word: string;
@@ -15,11 +11,54 @@ export class Piece extends Schema {
 
 export class Deck extends Schema {
 	@type({ array: Piece }) deck = new ArraySchema<Piece>();
+	constructor() {
+		super();
+		words.forEach((word, index) => {
+			const piece = new Piece();
+			piece.word = word;
+			piece.id = index;
+			this.deck.push(piece);
+		});
+		this.shuffle();
+	}
 	shuffle() {
 		for (let i = this.deck.length - 1; i > 0; i--) {
 			const j = Math.floor(Math.random() * (i + 1));
 			[this.deck[i], this.deck[j]] = [this.deck[j], this.deck[i]];
 		}
+	}
+	deal() {
+		console.log('Dealing a card...');
+		return this.deck.shift();
+	}
+}
+
+export class PromptCard extends Schema {
+	@type('string') prompt: string;
+	@type('number') id: number;
+}
+
+export class PromptDeck extends Schema {
+	@type({ array: PromptCard }) deck = new ArraySchema<PromptCard>();
+	constructor() {
+		super();
+		prompts.forEach((prompt, index) => {
+			const promptCard = new PromptCard();
+			promptCard.prompt = prompt;
+			promptCard.id = index;
+			this.deck.push(promptCard);
+		});
+		this.shuffle();
+	}
+	shuffle() {
+		for (let i = this.deck.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[this.deck[i], this.deck[j]] = [this.deck[j], this.deck[i]];
+		}
+	}
+	draw() {
+		console.log('Dealing a prompt...');
+		return this.deck.shift();
 	}
 }
 
@@ -29,14 +68,17 @@ export class PlayerCard extends Schema {
 
 export class Player extends Schema {
 	@type('string') name: string;
-	@type('number') id: number;
-	@type('string') userID: string;
 	@type({ array: Piece }) hand = new ArraySchema<Piece>();
+	@type('boolean') isVIP: boolean = false;
+	@type('string') status: 'waiting' | 'editing' | 'finished' = 'waiting';
 }
 
 export class MyRoomState extends Schema {
-	@type('string') mySynchronizedProperty: string = 'Hello world';
+	@type('string') roomId: string;
+	@type('string') gamePhase: 'lobby' | 'playing' = 'lobby';
+	@type('number') turn: number;
 	@type({ map: Player }) players = new MapSchema<Player>();
-	// @type({ map: Piece }) deck = new MapSchema<Piece>();
-  @type(Deck) deck = new Deck();
+	PromptDeck = new PromptDeck();
+	deck = new Deck();
+	@type(PromptCard) currentPrompt: PromptCard;
 }
