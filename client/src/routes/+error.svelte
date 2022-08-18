d88f15e6b91c5aa52785c7a84397f4b6d7bc3ff1
Luke Shafer <lukealanshafer@gmail.com>
2022-08-18 00:26:11 -0500

    svelte-migrate: renamed files, updated files

---
+++ client/src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
+	let status: number = $page.status;
+	let message = $page.error?.message;
+	onMount(() => {
+		if (status === 404) window.location.replace('/');
+	});
+</script>
+
+{#if status !== 404}
+	<h1>{status}</h1>
+	<p>{message}</p>
+{/if}
